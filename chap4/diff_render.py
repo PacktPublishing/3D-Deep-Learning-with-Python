3faa4a46552b012f5bee8ea3093a08a12c614a29
simon.mark <>
2021-12-29 20:59:41 -0800

    Codes for Chapter 4, a chapter on differentiable rendering. Implement one approach of silhoutte fitting and one approach of texture fitting.

---
+++ chap4/diff_render.py
@@ -0,0 +1,153 @@
+import cv2
+import os
+import torch
+import numpy as np
+from tqdm.notebook import tqdm
+import imageio
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from skimage import img_as_ubyte
+
+from pytorch3d.io import load_obj
+from pytorch3d.structures import Meshes
+from pytorch3d.transforms import Rotate, Translate
+
+from pytorch3d.renderer import (
+    FoVPerspectiveCameras, look_at_view_transform, look_at_rotation,
+    RasterizationSettings, MeshRenderer, MeshRasterizer, BlendParams,
+    SoftSilhouetteShader, HardPhongShader, PointLights, TexturesVertex,
+)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    torch.cuda.set_device(device)
+else:
+    device = torch.device("cpu")
+
+output_dir = './result_teapot'
+
+verts, faces_idx, _ = load_obj("./data/teapot.obj")
+faces = faces_idx.verts_idx
+
+verts_rgb = torch.ones_like(verts)[None]  # (1, V, 3)
+textures = TexturesVertex(verts_features=verts_rgb.to(device))
+
+teapot_mesh = Meshes(
+    verts=[verts.to(device)],
+    faces=[faces.to(device)],
+    textures=textures
+)
+
+cameras = FoVPerspectiveCameras(device=device)
+blend_params = BlendParams(sigma=1e-4, gamma=1e-4)
+
+raster_settings = RasterizationSettings(
+    image_size=256,
+    blur_radius=np.log(1. / 1e-4 - 1.) * blend_params.sigma,
+    faces_per_pixel=100,
+)
+
+silhouette_renderer = MeshRenderer(
+    rasterizer=MeshRasterizer(
+        cameras=cameras,
+        raster_settings=raster_settings
+    ),
+    shader=SoftSilhouetteShader(blend_params=blend_params)
+)
+
+raster_settings = RasterizationSettings(
+    image_size=256,
+    blur_radius=0.0,
+    faces_per_pixel=1,
+)
+
+lights = PointLights(device=device, location=((2.0, 2.0, -2.0),))
+phong_renderer = MeshRenderer(
+    rasterizer=MeshRasterizer(
+        cameras=cameras,
+        raster_settings=raster_settings
+    ),
+    shader=HardPhongShader(device=device, cameras=cameras, lights=lights)
+)
+
+distance = 3
+elevation = 50.0
+azimuth = 0.0
+
+R, T = look_at_view_transform(distance, elevation, azimuth, device=device)
+
+silhouette = silhouette_renderer(meshes_world=teapot_mesh, R=R, T=T)
+image_ref = phong_renderer(meshes_world=teapot_mesh, R=R, T=T)
+
+silhouette = silhouette.cpu().numpy()
+image_ref = image_ref.cpu().numpy()
+
+plt.figure(figsize=(10, 10))
+plt.imshow(silhouette.squeeze()[..., 3])  # only plot the alpha channel of the RGBA image
+plt.grid(False)
+plt.savefig(os.path.join(output_dir, 'target_silhouette.png'))
+
+plt.figure(figsize=(10, 10))
+plt.imshow(image_ref.squeeze())
+plt.grid(False)
+plt.savefig(os.path.join(output_dir, 'target_rgb.png'))
+
+class Model(nn.Module):
+    def __init__(self, meshes, renderer, image_ref):
+        super().__init__()
+        self.meshes = meshes
+        self.device = meshes.device
+        self.renderer = renderer
+
+        image_ref = torch.from_numpy((image_ref[..., :3].max(-1) != 1).astype(np.float32))
+        self.register_buffer('image_ref', image_ref)
+
+        self.camera_position = nn.Parameter(
+            torch.from_numpy(np.array([3.0, 6.9, +2.5], dtype=np.float32)).to(meshes.device))
+
+    def forward(self):
+        R = look_at_rotation(self.camera_position[None, :], device=self.device)  # (1, 3, 3)
+        T = -torch.bmm(R.transpose(1, 2), self.camera_position[None, :, None])[:, :, 0]  # (1, 3)
+
+        image = self.renderer(meshes_world=self.meshes.clone(), R=R, T=T)
+        loss = torch.sum((image[..., 3] - self.image_ref) ** 2)
+        return loss, image
+
+model = Model(meshes=teapot_mesh, renderer=silhouette_renderer, image_ref=image_ref).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.05)
+
+_, image_init = model()
+plt.figure(figsize=(10, 10))
+plt.imshow(image_init.detach().squeeze().cpu().numpy()[..., 3])
+plt.grid(False)
+plt.title("Starting Silhouette")
+plt.savefig(os.path.join(output_dir, 'starting_silhouette.png'))
+
+for i in range(0, 200):
+    print('i = ', i)
+
+    optimizer.zero_grad()
+    loss, _ = model()
+    loss.backward()
+    optimizer.step()
+
+    if loss.item() < 500:
+        break
+
+
+    R = look_at_rotation(model.camera_position[None, :], device=model.device)
+    T = -torch.bmm(R.transpose(1, 2), model.camera_position[None, :, None])[:, :, 0]  # (1, 3)
+    image = phong_renderer(meshes_world=model.meshes.clone(), R=R, T=T)
+    image = image[0, ..., :3].detach().squeeze().cpu().numpy()
+    image = img_as_ubyte(image)
+
+    plt.figure()
+    plt.imshow(image[..., :3])
+    plt.title("iter: %d, loss: %0.2f" % (i, loss.data))
+    plt.axis("off")
+    plt.savefig(os.path.join(output_dir, 'fitting_' + str(i) + '.png'))
+
+print('Finished')
+
+
---
+++ chap4/diff_render_texture.py
@@ -0,0 +1,200 @@
+import cv2
+import os
+import torch
+import numpy as np
+import imageio
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from skimage import img_as_ubyte
+
+from pytorch3d.io import load_objs_as_meshes, load_obj
+from pytorch3d.structures import Meshes
+from pytorch3d.transforms import Rotate, Translate
+
+from pytorch3d.renderer import (
+    FoVPerspectiveCameras, look_at_view_transform, look_at_rotation,
+    RasterizationSettings, MeshRenderer, MeshRasterizer, BlendParams,
+    SoftSilhouetteShader, HardPhongShader, PointLights, TexturesVertex,
+    SoftPhongShader
+)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    torch.cuda.set_device(device)
+else:
+    device = torch.device("cpu")
+
+output_dir = './result_cow'
+
+obj_filename = "./data/cow_mesh/cow.obj"
+cow_mesh = load_objs_as_meshes([obj_filename], device=device)
+
+cameras = FoVPerspectiveCameras(device=device)
+lights = PointLights(device=device, location=((2.0, 2.0, -2.0),))
+
+blend_params = BlendParams(sigma=1e-4, gamma=1e-4)
+raster_settings = RasterizationSettings(
+    image_size=256,
+    blur_radius=np.log(1. / 1e-4 - 1.) * blend_params.sigma,
+    faces_per_pixel=100,
+)
+
+renderer_silhouette = MeshRenderer(
+    rasterizer=MeshRasterizer(
+        cameras=cameras,
+        raster_settings=raster_settings
+    ),
+    shader=SoftSilhouetteShader(blend_params=blend_params)
+)
+
+sigma = 1e-4
+raster_settings_soft = RasterizationSettings(
+    image_size=256,
+    blur_radius=np.log(1. / 1e-4 - 1.)*sigma,
+    faces_per_pixel=50,
+)
+renderer_textured = MeshRenderer(
+    rasterizer=MeshRasterizer(
+        cameras=cameras,
+        raster_settings=raster_settings_soft
+    ),
+    shader=SoftPhongShader(device=device,
+        cameras=cameras,
+        lights=lights)
+)
+
+raster_settings = RasterizationSettings(
+    image_size=256,
+    blur_radius=0.0,
+    faces_per_pixel=1,
+)
+phong_renderer = MeshRenderer(
+    rasterizer=MeshRasterizer(
+        cameras=cameras,
+        raster_settings=raster_settings
+    ),
+    shader=HardPhongShader(device=device, cameras=cameras, lights=lights)
+)
+
+distance = 3
+elevation = 50.0
+azimuth = 0.0
+R, T = look_at_view_transform(distance, elevation, azimuth, device=device)
+
+silhouette = renderer_silhouette(meshes_world=cow_mesh, R=R, T=T)
+image_ref = phong_renderer(meshes_world=cow_mesh, R=R, T=T)
+silhouette = silhouette.cpu().numpy()
+image_ref = image_ref.cpu().numpy()
+
+plt.figure(figsize=(10, 10))
+plt.imshow(silhouette.squeeze()[..., 3])
+plt.grid(False)
+plt.savefig(os.path.join(output_dir, 'target_silhouette.png'))
+
+plt.figure(figsize=(10, 10))
+plt.imshow(image_ref.squeeze())
+plt.grid(False)
+plt.savefig(os.path.join(output_dir, 'target_rgb.png'))
+
+class Model(nn.Module):
+    def __init__(self, meshes, renderer_silhouette, renderer_textured, image_ref, weight_silhouette, weight_texture):
+        super().__init__()
+        self.meshes = meshes
+        self.device = meshes.device
+        self.renderer_silhouette = renderer_silhouette
+        self.renderer_textured = renderer_textured
+
+        self.weight_silhouette = weight_silhouette
+        self.weight_texture = weight_texture
+
+        image_ref_silhouette = torch.from_numpy((image_ref[..., :3].max(-1) != 1).astype(np.float32))
+        self.register_buffer('image_ref_silhouette', image_ref_silhouette)
+
+        image_ref_textured = torch.from_numpy((image_ref[..., :3]).astype(np.float32))
+        self.register_buffer('image_ref_textured', image_ref_textured)
+
+        self.camera_position = nn.Parameter(
+            torch.from_numpy(np.array([3.0, 6.9, +2.5], dtype=np.float32)).to(meshes.device))
+
+    def forward(self):
+        # Render the image using the updated camera position. Based on the new position of the
+        # camera we calculate the rotation and translation matrices
+        R = look_at_rotation(self.camera_position[None, :], device=self.device)  # (1, 3, 3)
+        T = -torch.bmm(R.transpose(1, 2), self.camera_position[None, :, None])[:, :, 0]  # (1, 3)
+
+        image_silhouette = self.renderer_silhouette(meshes_world=self.meshes.clone(), R=R, T=T)
+        image_textured = self.renderer_textured(meshes_world=self.meshes.clone(), R=R, T=T)
+
+        loss_silhouette = torch.sum((image_silhouette[..., 3] - self.image_ref_silhouette) ** 2)
+        loss_texture = torch.sum((image_textured[..., :3] - self.image_ref_textured) ** 2)
+
+        loss = self.weight_silhouette * loss_silhouette + self.weight_texture * loss_texture
+        return loss, image_silhouette, image_textured
+
+model = Model(meshes=cow_mesh, renderer_silhouette=renderer_silhouette, renderer_textured = renderer_textured,
+              image_ref=image_ref, weight_silhouette=1.0, weight_texture=0.1).to(device)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.05)
+
+_, image_silhouette_init, image_rgb_init = model()
+plt.figure(figsize=(10, 10))
+plt.imshow(image_silhouette_init.detach().squeeze().cpu().numpy()[..., 3])
+plt.grid(False)
+plt.title("Starting Silhouette")
+plt.savefig(os.path.join(output_dir, 'starting_silhouette.png'))
+
+plt.figure(figsize=(10, 10))
+plt.imshow(image_rgb_init.cpu().detach().numpy().squeeze())
+plt.grid(False)
+plt.title("Starting RGB Image");
+plt.savefig(os.path.join(output_dir, 'starting_rgb.png'))
+
+for i in range(0, 200):
+    print('i = ', i)
+
+    optimizer.zero_grad()
+    loss, image_silhouette, image_textured = model()
+    loss.backward()
+    optimizer.step()
+
+    plt.figure()
+    plt.imshow(image_silhouette[..., 3].detach().squeeze().cpu().numpy())
+    plt.title("iter: %d, loss: %0.2f" % (i, loss.data))
+    plt.axis("off")
+    plt.savefig(os.path.join(output_dir, 'soft_silhouette_' + str(i) + '.png'))
+
+    plt.figure()
+    plt.imshow(image_textured.detach().squeeze().cpu().numpy())
+    plt.title("iter: %d, loss: %0.2f" % (i, loss.data))
+    plt.axis("off")
+    plt.savefig(os.path.join(output_dir, 'soft_texture_' + str(i) + '.png'))
+
+    R = look_at_rotation(model.camera_position[None, :], device=model.device)
+    T = -torch.bmm(R.transpose(1, 2), model.camera_position[None, :, None])[:, :, 0]  # (1, 3)
+    image = phong_renderer(meshes_world=model.meshes.clone(), R=R, T=T)
+
+    plt.figure()
+    plt.imshow(image[..., 3].detach().squeeze().cpu().numpy())
+    plt.title("iter: %d, loss: %0.2f" % (i, loss.data))
+    plt.axis("off")
+    plt.savefig(os.path.join(output_dir, 'hard_silhouette_' + str(i) + '.png'))
+
+    image = image[0, ..., :3].detach().squeeze().cpu().numpy()
+    image = img_as_ubyte(image)
+
+    plt.figure()
+    plt.imshow(image[..., :3])
+    plt.title("iter: %d, loss: %0.2f" % (i, loss.data))
+    plt.axis("off")
+    plt.savefig(os.path.join(output_dir, 'hard_texture_' + str(i) + '.png'))
+
+    if loss.item() < 800:
+        break
+
+print('Finished')
+
+
+
+
+
